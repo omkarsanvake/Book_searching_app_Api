d9d3e844fe7676362f62c7a092e0f8740a413ee6
omkarsanvake <omkarsanvake555@gmail.com>
2023-09-29 17:58:01 +0530

    first commit

---
+++ src/Pages/Home/Home.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import Header from '../../Component/Header/Header';
+import { Outlet } from 'react-router-dom';
+
+
+const Home = () => {
+  return (
+    <main>
+    <Header/>
+    <Outlet/>
+
+    </main>
+  )
+}
+
+export default Home
+
--- src/index.js
+++ src/index.js
@@ -1,17 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { AppProvider } from './Context';
+import Home from './Pages/Home/Home';
+import About from './Pages/About/About'
+import BookDetails from "./Component/BookDetails/Bookdetails";
+import BookList from "./Component/BookList/BookList";
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <AppProvider>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Home />}></Route>
+        <Route path='about' element={<About />}></Route>
+        <Route path='book' element={<BookList />} />
+        <Route path='book/:id' element={<BookDetails />} />
+
+
+      </Routes>
+    </BrowserRouter>
+  </AppProvider>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
